7aee116afe936836fd3fc215240db27e75e3495c
LuanGale <lgaletascon@gmail.com>
2025-08-07 10:13:32 +0200

    change example + change result apperance

--- src/index.js
+++ src/index.js
@@ -17,8 +17,10 @@ function generateRecipe(event) {
   let prompt = `Generate an easy recipe with ${ingredientInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let delayAnswer = document.querySelector("#result");
-  delayAnswer.innerHTML = `Generating a recipe with ${ingredientInput.value}... please wait`;
+  let appearElement = document.querySelector("#hidden");
+  appearElement.classList.remove("hidden");
+
+  appearElement.innerHTML = `Generating a recipe with ${ingredientInput.value}... please wait`;
   axios.get(apiUrl).then(answerRecipe);
 }
 
